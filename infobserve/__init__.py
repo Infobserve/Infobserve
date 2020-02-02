175b1a373a5314f7f6cf0deb73cabad2518f25f7
Kostas Kolivas <kwstaskolivas@gmail.com>
2020-02-02 15:49:38 +0000

    Adds YaraProcessor consumer.
    * YaraProcessor consumes the sources_queue.
    * YaraProcessor cmd_queue feature.
    * YaraProcessor hot reload of rules.
    
    Also adds stop function to YaraProcessor
    INFO-18

--- infobserve/__init__.py
+++ infobserve/__init__.py
@@ -6,6 +6,7 @@ from .common import CONFIG
 from .common import APP_LOGGER
 from .common.queue import ProcessingQueue
 from .sources import SOURCE_FACTORY
+from .processors.yara_processor import YaraProcessor
 
 __version__ = '0.1.0'
 
@@ -18,29 +19,40 @@ def init_sources(config):
     return sources
 
 
-async def log_consumer(queue):
-    while True:
-        event = await queue.get_event()
-        APP_LOGGER.debug("Consumed Timestamp:%s File:%s Size:%s Creator:%s ", event.timestamp, event.filename,
-                         event.size, event.creator)
-
-
-def source_scheduler(sources, loop, processing_queue, pool=None):
+def source_scheduler(sources, loop, source_queue, pool=None):
     for source in sources:
         APP_LOGGER.debug("Scheduling Source:%s", source.name)
-        loop.create_task(source.fetch_events_scheduled(processing_queue, pool))
+        loop.create_task(source.fetch_events_scheduled(source_queue, pool))
 
     return loop
 
 
-def consumer_scheduler():
-    pass
+def consumer_scheduler(loop, source_queue, db_queue):
+    """
+    Creates a YaraProcessor, passing it the Yara rule file paths as read from the config file.
+
+    Args:
+        loop (asyncio loop): The loop to add the consumer as a task to
+        source_queue (infobserve.common.queue.ProcessingQueue): The queue from which the processor will retrieve
+                                                                sources
+        db_queue (infobserve.common.queue.ProcessingQueue): The queue into which the processor will place any
+                                                            matches
+    """
+    APP_LOGGER.debug("Starting Yara Processor")
+    consumer = YaraProcessor(CONFIG.YARA_RULES_PATHS, source_queue, db_queue)
+    loop.create_task(consumer.process())
+
+    return loop
 
 
 def main():
     APP_LOGGER.info("Logging up and running")
     APP_LOGGER.debug("Configured Sources:%s", CONFIG.SOURCES)
-    processing_queue = ProcessingQueue(CONFIG.PROCESSING_QUEUE_SIZE)
+    source_queue = ProcessingQueue(CONFIG.PROCESSING_QUEUE_SIZE)
+    # TODO: Add DB queue size option in the config?
+    db_queue = ProcessingQueue()
+
+    # Initialize Yara Processing queue
 
     main_loop = asyncio.get_event_loop()
 
@@ -49,8 +61,9 @@ def main():
     main_loop.run_until_complete(CONFIG.init_db(pg_pool))
     APP_LOGGER.info("Initialized Schema")
 
-    main_loop = source_scheduler(init_sources(CONFIG.SOURCES), main_loop, processing_queue, pg_pool)
-    main_loop.create_task(log_consumer(processing_queue))
+    main_loop = source_scheduler(init_sources(CONFIG.SOURCES), main_loop, source_queue, pg_pool)
+    main_loop = consumer_scheduler(main_loop, source_queue, db_queue)
+
     APP_LOGGER.debug("Consumer Scheduled")
     APP_LOGGER.info("Main Loop Initialized")
     main_loop.run_forever()
